0aa880cca1b7f705421b2c8a13c13c18a3d0efb2
Takashi Kitajima <inc@2inc.org>
2018-12-25 11:05:59 +0900

    Snow Monkey v5 compatibility

--- bootstrap.php
+++ bootstrap.php
@@ -5,46 +5,79 @@
  * @license GPL-2.0+
  */
 
-add_filter( 'theme_mod_accent-color', function( $option ) {
-	return '#ee85b5';
-} );
+add_filter(
+	'theme_mod_accent-color',
+	function( $option ) {
+		return '#ee85b5';
+	}
+);
 
-add_filter( 'theme_mod_archive-page-layout', function( $option ) {
-	return 'right-sidebar';
-} );
+add_filter(
+	'theme_mod_archive-page-layout',
+	function( $option ) {
+		return 'right-sidebar';
+	}
+);
 
-add_filter( 'theme_mod_page-layout', function( $option ) {
-	return 'right-sidebar';
-} );
+add_filter(
+	'theme_mod_page-layout',
+	function( $option ) {
+		return 'right-sidebar';
+	}
+);
 
-add_filter( 'theme_mod_header-layout', function( $option ) {
-	return '1row';
-} );
+add_filter(
+	'theme_mod_header-layout',
+	function( $option ) {
+		return '1row';
+	}
+);
 
-add_filter( 'theme_mod_breadcrumbs-position', function( $option ) {
-	return 'content-width';
-} );
+add_filter(
+	'theme_mod_breadcrumbs-position',
+	function( $option ) {
+		return 'content-width';
+	}
+);
 
-add_filter( 'theme_mod_header-position', function( $option ) {
-	return 'overlay';
-} );
+add_filter(
+	'theme_mod_header-position',
+	function( $option ) {
+		return 'overlay';
+	}
+);
 
-add_filter( 'theme_mod_header-position-only-mobile', function( $option ) {
-	return false;
-} );
+add_filter(
+	'theme_mod_header-position-only-mobile',
+	function( $option ) {
+		return false;
+	}
+);
 
-add_filter( 'theme_mod_default-page-header-image', function( $image ) {
-	return plugins_url( 'img/pink-1326168_1920.jpg', __FILE__ );
-} );
+add_filter(
+	'theme_mod_default-page-header-image',
+	function( $image ) {
+		return plugins_url( 'img/pink-1326168_1920.jpg', __FILE__ );
+	}
+);
 
-add_filter( 'theme_mod_post-eyecatch', function( $image ) {
-	return 'page-header';
-} );
+add_filter(
+	'theme_mod_post-eyecatch',
+	function( $image ) {
+		return 'page-header';
+	}
+);
 
-add_filter( 'theme_mod_page-eyecatch', function( $image ) {
-	return 'page-header';
-} );
+add_filter(
+	'theme_mod_page-eyecatch',
+	function( $image ) {
+		return 'page-header';
+	}
+);
 
-add_filter( 'snow_monkey_page_header_thumbnail_size', function( $size ) {
-	return 'full';
-} );
+add_filter(
+	'snow_monkey_page_header_thumbnail_size',
+	function( $size ) {
+		return 'full';
+	}
+);
--- snow-monkey-design-skin-spring.php
+++ snow-monkey-design-skin-spring.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: [ Snow Monkey Design Skin ] Spring
  * Description: A design skin of the Snow Monkey.
- * Version: 0.2.1
+ * Version: 0.2.2
  * Text Domain: snow-monkey-design-skin-spring
  * Domain Path: /languages/
  *
@@ -15,24 +15,44 @@ use Inc2734\WP_GitHub_Plugin_Updater\GitHub_Plugin_Updater;
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
-add_action( 'plugins_loaded', function() {
-	load_plugin_textdomain( 'snow-monkey-design-skin-spring', false, basename( __DIR__ ) . '/languages' );
+add_action(
+	'plugins_loaded',
+	function() {
+		load_plugin_textdomain( 'snow-monkey-design-skin-spring', false, basename( __DIR__ ) . '/languages' );
 
-	add_action( 'init', function() {
-		new GitHub_Plugin_Updater( plugin_basename( __FILE__ ), 'inc2734', 'snow-monkey-design-skin-spring' );
-	} );
+		add_action(
+			'init',
+			function() {
+				new GitHub_Plugin_Updater( plugin_basename( __FILE__ ), 'inc2734', 'snow-monkey-design-skin-spring' );
+			}
+		);
 
-	add_filter( 'snow_monkey_design_skin_choices', function( $choices ) {
-		$plugin_data = get_file_data( __FILE__, [
-			'label' => 'Plugin Name',
-		], 'plugin');
-		$choices[ basename( __FILE__, '.php' ) ] = $plugin_data['label'];
-		return $choices;
-	} );
-} );
-
-add_action( 'after_setup_theme', function() {
-	if ( class_exists( '\Snow_Monkey\app\model\Design_Skin' ) ) {
-		new \Snow_Monkey\app\model\Design_Skin( __FILE__ );
+		add_filter(
+			'snow_monkey_design_skin_choices',
+			function( $choices ) {
+				$plugin_data = get_file_data(
+					__FILE__,
+					[
+						'label' => 'Plugin Name',
+					],
+					'plugin'
+				);
+				$choices[ basename( __FILE__, '.php' ) ] = $plugin_data['label'];
+				return $choices;
+			}
+		);
 	}
-} );
+);
+
+add_action(
+	'after_setup_theme',
+	function() {
+		if ( class_exists( '\Snow_Monkey\app\model\Design_Skin' ) ) {
+			new \Snow_Monkey\app\model\Design_Skin( __FILE__ );
+		}
+
+		if ( class_exists( '\Framework\Model\Design_Skin' ) ) {
+			new \Framework\Model\Design_Skin( __FILE__ );
+		}
+	}
+);
