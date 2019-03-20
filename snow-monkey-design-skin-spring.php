9a2898b1126b330bd35f23bcc9a6f8c90adb39cb
Takashi Kitajima <inc@2inc.org>
2019-03-20 11:33:49 +0900

    0.2.3

--- snow-monkey-design-skin-spring.php
+++ snow-monkey-design-skin-spring.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: [ Snow Monkey Design Skin ] Spring
  * Description: A design skin of the Snow Monkey.
- * Version: 0.2.2
+ * Version: 0.2.3
  * Text Domain: snow-monkey-design-skin-spring
  * Domain Path: /languages/
  *
