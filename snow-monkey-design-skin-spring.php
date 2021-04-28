86c600f8874edaa81e9b9b3a1aa44fdc487d8dfc
inc2734 <inc@2inc.org>
2021-04-28 19:37:05 +0900

    3.0.0

--- snow-monkey-design-skin-spring.php
+++ snow-monkey-design-skin-spring.php
@@ -2,8 +2,8 @@
 /**
  * Plugin name: [ Snow Monkey Design Skin ] Spring
  * Description: A design skin of the Snow Monkey.
- * Version: 2.1.1
- * Tested up to: 5.5
+ * Version: 3.0.0
+ * Tested up to: 5.7
  * Requires at least: 5.5
  * Requires PHP: 5.6
  * Text Domain: snow-monkey-design-skin-spring
